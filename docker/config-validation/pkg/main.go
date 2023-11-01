0733448a62c79ee7b8e476c275c2ca09c6a93470
nikunis <nik.unisikhin.05@gmail.com>
2023-11-01 23:26:23 +0500

    add first test

---
+++ docker/config-validation/pkg/main.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+func currentWorkers() error {
+	ctx := context.TODO()
+
+	var err error
+	out, err := exec.CommandContext(ctx, "/usr/bin/odyssey /etc/odyssey/odyssey-test.conf --test").Output()
+
+	if err != nil {
+		return err
+	}
+
+	if strOut := string(out); !strings.Contains(strOut, "config is valid") {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	if err := currentWorkers(); err != nil {
+		fmt.Println("error: current workers field is not pass")
+	} else {
+		log.Println("workers: Ok")
+	}
+}
