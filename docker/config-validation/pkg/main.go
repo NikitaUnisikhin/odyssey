7350bc029d97ce9948a890e907c6326ecbcf67da
nikunis <nik.unisikhin.05@gmail.com>
2023-11-02 10:24:14 +0500

    add noCurrentWorkers test

--- docker/config-validation/pkg/main.go
+++ docker/config-validation/pkg/main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
 )
+
+const filePath = "/etc/odyssey/odyssey-test.conf"
+const newFilePath = "/etc/odyssey/odyssey-new-test.conf"
+
+func changeConfig(prefix string, stringToReplace string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.HasPrefix(scanner.Text(), prefix) {
+			text = stringToReplace
+		}
+
+		lines = append(lines, text)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(newFilePath, []byte(strings.Join(lines, "\n")), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func currentWorkers() error {
 	ctx := context.TODO()
@@ -25,10 +60,34 @@ func currentWorkers() error {
 	return nil
 }
 
+func noCurrentWorkers() error {
+	ctx := context.TODO()
+
+	err := changeConfig("workers", "workers -1")
+
+	if err != nil {
+		return err
+	}
+
+	out, err := exec.CommandContext(ctx, "/usr/bin/odyssey /etc/odyssey/odyssey-new-test.conf --test").Output()
+
+	if err != nil {
+		return err
+	}
+
+	if strOut := string(out); !strings.Contains(strOut, "bad workers number") {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-	if err := currentWorkers(); err != nil {
+	if err := currentWorkers(); err == nil {
 		fmt.Println("error: current workers field is not pass")
+	} else if err := noCurrentWorkers(); err == nil {
+		fmt.Println("error: no current workers field is pass")
 	} else {
-		log.Println("workers: Ok")
+		fmt.Println("workers: Ok")
 	}
 }
