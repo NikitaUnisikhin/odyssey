9865ac583585fcf0f02cfc1abda2cd8f4b470fe7
nikunis <nik.unisikhin.05@gmail.com>
2023-11-24 18:21:55 +0500

    fix

--- sources/rules.c
+++ sources/rules.c
@@ -827,7 +827,8 @@ int od_rules_autogenerate_defaults(od_rules_t *rules, od_logger_t *logger)
 		if (rule->auth_query != NULL &&
 		    !od_rules_match(rules, rule->db_name, rule->user_name,
 				    rule->user_ip, rule->db_is_default,
-				    rule->user_is_default, rule->user_ip_is_default
+				    rule->user_is_default,
+				    rule->user_ip_is_default
 				    1)) {
 			need_autogen = true;
 			break;
